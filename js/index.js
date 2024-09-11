76a07f1d62e86b385e44f2106876bfce75793876
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-09-11 06:15:29 +0200

    #init commit, header was created

---
+++ js/index.js
@@ -0,0 +1,18 @@
+//При нажатии на header__Burger должен добавиться класс к header__burger и  header__menu
+// К боди должен добавиться класс lock чтоб под бургером не скролилось меню
+
+const headerBurger = document.querySelector('.header__burger');
+
+const headerMenu = document.querySelector('.header__menu');
+const header = document.querySelector('.header');
+const body = document.body;
+
+
+const activeBurger = ()=>{
+   headerBurger.classList.toggle('active');
+   headerMenu.classList.toggle('active');
+   header.classList.toggle('active');
+   body.classList.toggle('lock');
+}
+
+headerBurger.addEventListener('click', activeBurger);
